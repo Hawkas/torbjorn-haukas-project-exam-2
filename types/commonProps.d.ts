c75867c93ae292281de58a377d5fe1f4dc7304a2
Torbjørn Haukås <69842749+Hawkas@users.noreply.github.com>
2022-06-16 02:00:58 +0200

    well then

--- types/commonProps.d.ts
+++ types/commonProps.d.ts
@@ -1,11 +1,10 @@
 import type { AccommodationClean } from './accommodationClean';
-import type { Accommodations } from './accommodationRaw';
 import type { BookingCleaned } from './bookings';
 
 export interface AdminProps {
-  bookings: BookingCleaned[] | null;
+  bookings: BookingCleaned[] | [];
   data: AccommodationClean[] | null;
-  messageData: Message[];
+  messageData: Message[] | [];
 }
 
 export interface DataProps {
